ea27dca1d652ff977e46bc42e62ac45b0aad5a2b
Coding <coding@D-10-18-250-207.dhcp4.washington.edu>
2019-01-30 18:12:53 -0800

    updated ajax call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,10 @@
-function whatever(){
-    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How+much+vitamin+c+is+in+2+apples%3F";
+// function whatever(){
 
-$.ajax({
-    // header: {
-    //     'X-RapidAPI-Key': '4e620d42c0mshd5bc66baae8748dp176b3djsn9f8470266a81'
-    // },
-    url: queryURL,
-    method: "GET",
-    beforeSend: function(xhr){
-        xhr.setRequestHeader('X-RapidAPI-Key', '4e620d42c0mshd5bc66baae8748dp176b3djsn9f8470266a81');
-    }
-}).then(function (results) {
-    console.log(results)
 
-});
+// });
 
-}
-whatever();
+// }
+// whatever();
 
 
 
@@ -25,11 +13,20 @@ whatever();
 // nutrition API
 $('#get-started-button').on('click', function () {
     console.log('test');
+    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How+much+vitamin+c+is+in+2+apples%3F";
 
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-RapidAPI-Key', '4e620d42c0mshd5bc66baae8748dp176b3djsn9f8470266a81');
+        }
+    }).then(function (results) {
+        console.log(results)
+
+    });
 
 });
-
-
 
 // $("#search-button").on("click", function () {
 //     var apiKey = "&api_key=dc6zaTOxFJmzC";
@@ -64,6 +61,3 @@ $('#get-started-button').on('click', function () {
 
 
 // });
-
-
-
